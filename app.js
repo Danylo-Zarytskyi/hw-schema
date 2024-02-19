b0e745a694ad4f275a566c379d3892150aa35e1f
danylo <z1keldanulo281@gmail.com>
2024-02-19 19:05:50 +0200

    second commit

--- app.js
+++ app.js
@@ -5,31 +5,21 @@ const ajv = new Ajv();
 const schema = {
     type: "object",
     properties: {
-        a: {type: "integer"},
-        b: {type: "string"},
-        c: {type: "number"},
-        bam: {
-            type: 'array',
-            minItems: 1,
-            maxItems: 3,
-            items: {
-                type: 'number',
-            }
-            
-        },
+        postId: {type: "integer"},
+        id: {type: "integer"},
+        name: {type: "string"},
+        email: {type: "string"},
+        body: {type: "string"}
     }
 }
 
 
 const data = {
-    a: 10,
-    b: "20",
-    c: 10.20,
-    bam: [
-        {"item1":  10},
-        {"item2":  20},
-        {"item3":  30}
-    ]
+    "postId": 1,
+    "id": 1,
+    "name": "id labore ex et quam laborum",
+    "email": "Eliseo@gardner.biz",
+    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
 }
 
 const validate = ajv.compile(schema)
